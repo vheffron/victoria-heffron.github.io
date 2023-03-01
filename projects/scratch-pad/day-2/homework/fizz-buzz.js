d00dc416f929d29e62a30e5d02b93fef23cb9377
Victoria H <victoria.heffron@gmail.com>
2023-03-01 21:18:15 +0000

    finished day 2 hw

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -13,13 +13,13 @@
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
     for (var i = 1; i <= 100; i++){
-        if (i % 3 && i % 5){
+        if (i % 3 === 0 && i % 5 === 0){
             console.log("FizzBuzz")
         }
-        else if (i % 3){
+        else if (i % 3 === 0){
             console.log("Fizz")
         }
-        else if (i % 5){
+        else if (i % 5 === 0){
             console.log("Buzz")
         }
         else {console.log(i)}
