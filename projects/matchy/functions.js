d34873dbc61558f04532b567de09990c71b9da3c
Victoria H <victoria.heffron@gmail.com>
2023-03-08 00:22:58 +0000

    saving progress

--- projects/matchy/functions.js
+++ projects/matchy/functions.js
@@ -13,13 +13,34 @@
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+//declare function called search that takes a paramater representing an array of animals
+//and a parameter representing a string, the name of an animal on which to perform a search 
+function search(animals, animalName){
+    //looks through animals array 
+    for (var i = 0; i < animals.length; i++){
+    //returns animal's object if animal name exists
+    var animalObject = animals[i]
+    if (animalName === animalObject.name){
+        return animalObject
+    }
+    } return null
+    }
+   
 
 //////////////////////////////////////////////////////////////////////
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+function replace(animals, name, replacement){
+    //loop through array to check each name
+    for (var i = 0; i < animals.length; i++){
+    //if name of animal exists in the array
+        if(animals[i].name === name){
+        //replace its object with the replacement object
+            animals[i] = replacement 
+        }
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
