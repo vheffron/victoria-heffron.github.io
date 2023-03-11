bd7982aa9240ecfcbd3ab29b6dbdff71cffe1656
Victoria H <victoria.heffron@gmail.com>
2023-03-11 02:18:11 +0000

    saving progress

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -31,8 +31,22 @@ function fizzBuzz(numberOne, numberTwo) {
 // drawChessboard //////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
-function drawChessboard() {
-
+function drawChessboard(n) {
+  let size = n;
+  let board = "";
+  
+  for (let y = 0; y < size; y++) {
+    for (let x = 0; x < size; x++) {
+      if ((x + y) % 2 == 0) {
+        board += " ";
+      } else {
+        board += "#";
+      }
+    }
+    board += "\n";
+  }
+  
+  console.log(board);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
