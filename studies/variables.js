09dfe73c8bbe5484d876177006c3172898b9025b
Victoria H <victoria.heffron@gmail.com>
2023-03-16 18:35:27 +0000

    saving progress

--- studies/variables.js
+++ studies/variables.js
@@ -4,7 +4,7 @@
  * 0: [5 sentence info] 
  * 
  * 1: Declaration and assignment: t ocreate a variable in js, it is best practice to first declare the 
- * variable using on of the three declarative keywords. once declared, you can then assign the variable a 
+ * variable using on of the three declarative keywords: var, let, or const. once declared, you can then assign the variable a 
  * value
  * 
  * 2. Var, let, const,
@@ -14,7 +14,7 @@
  */
 
 //1// Declaration and Assignment [code examples for every section]
-var age; //variable age delcated
+var age; //variable age delcared
 age = 35 // variable assigned value of 35
 
 //2// 
