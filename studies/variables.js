04ebb7d4036bf6832a73c895df611110fe31930b
Victoria H <victoria.heffron@gmail.com>
2023-03-17 20:22:08 +0000

    saving progress

--- studies/variables.js
+++ studies/variables.js
@@ -1,20 +1,29 @@
 /**
  * VARIABLES:
  * 
- * 0: [5 sentence info] 
+ * 0: Variables traditonally thought of as containers for values. Alternately, they can be thought of something more like 
+ * octopus tentacles that point at certain values. This metaphor allows us to think of variables as something that can be 
+ * redefined (the tentacle can stop pointing at one value andm move to point at another value) and shows that multiple variables
+ * can be pointed at the same value. 
  * 
- * 1: Declaration and assignment: t ocreate a variable in js, it is best practice to first declare the 
- * variable using on of the three declarative keywords: var, let, or const. once declared, you can then assign the variable a 
- * value
+ * 1: Declaration and assignment: to create a variable in js, it is best practice to first declare the variable using 
+ * one of the three declarative keywords: var, let, or const. once declared, you can then assign the variable a 
+ * value.
  * 
- * 2. Var, let, const,
+ * 2. Var, let, and: The three variable declaration keywords have various uses and benefits. Var, the original variable declaration
+ * keyword, was the only option until ES6, when let and const were introduced to address some of var's limitations.  
  * 
  * 3. Hoisting
  * 
  */
 
-//1// Declaration and Assignment [code examples for every section]
-var age; //variable age delcared
+//1// Declaration and Assignment 
+var age; //variable age declared
 age = 35 // variable assigned value of 35
 
-//2// 
+
+//2// Var, let, and const
+
+
+
+//3//Hoisting 
