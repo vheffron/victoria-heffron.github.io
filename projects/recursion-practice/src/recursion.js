00ed8bad0b796edfc6384697373b3345f53c3f49
Victoria H <victoria.heffron@gmail.com>
2023-03-22 20:18:41 +0000

    saving progress

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -164,7 +164,16 @@ var reverse = function (string, newString = '') {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function (string) {
-};
+  string = string.replace(/[^a-z0-9]/i, '').toLowerCase();
+  if (string.length <= 1){
+  return true
+  }
+  if (string[0] !== string[string.length - 1]){ 
+    return false
+  }
+  return palindrome(string.slice(1, -1))
+  }
+  
 
 // 11. Write a function that returns the remainder of x divided by y without using the
 // modulo (%) operator.
