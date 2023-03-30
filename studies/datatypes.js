adeba6996a3b405b10dd198a6da30347f8604f66
Victoria H <victoria.heffron@gmail.com>
2023-03-30 01:18:40 +0000

    saving progress

--- studies/datatypes.js
+++ studies/datatypes.js
@@ -1,9 +1,10 @@
 /**
  * DATATYPES
  * 
- * 0: 
+ * 0: Datatype are the categories that various values fall into. Below is a complete list of data types in JavaScript,
+ * as well as explanations of how the various datatypes behave when copied. 
  * 
- * 1: Number:
+ * 1: Number: Numbers are 
  * 
  * 2: String:
  * 
@@ -25,7 +26,7 @@
  * 
  * 11: Difference between primitive and complex data types: 
  * 
- * 12: Copy by value/Copy by reference: 
+ * 12: Copy by value (primitive)/Copy by reference (complex): 
  * 
  * 
  * 
@@ -33,4 +34,27 @@
  * 
  */
 
-//1// 
+1: Number: Numbers are 
+ * 
+ * 2: String:
+ * 
+ * 3: Boolean:
+ * 
+ * 4: Array: 
+ * 
+ * 5: Object:
+ * 
+ * 6: Function: 
+ * 
+ * 7: undefined:
+ * 
+ * 8: null:
+ * 
+ * 9: NaN
+ * 
+ * 10: Infinity and -Infinity
+ * 
+ * 11: Difference between primitive and complex data types: 
+ * 
+ * 12: Copy by value (primitive)/Copy by reference (complex): 
+
