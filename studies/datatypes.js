f2f2d51bf847588f932b87d2919b2f2659799e68
Victoria H <victoria.heffron@gmail.com>
2023-03-31 18:37:08 +0000

    saving progress

--- studies/datatypes.js
+++ studies/datatypes.js
@@ -13,6 +13,7 @@
  * the original string 
  * 
  * 3: Boolean: There are two Boolean values: true and false. These represent the logical outcome of conditional operations like if statements. 
+ * Booleans also represent whether a given statement is "truthy" or "falsy."
  * 
  * 4: Array: Arrays are complex datatypes that store data as indexed lists. They also have a length property. Beause of this, there are a variety 
  * of methods that can be performed on them to manipulate then. They can be looped over, allowing us to perform an action on every item in the 
@@ -33,36 +34,61 @@
  * is less than any other number. 
  * 
  * 11: Difference between primitive and complex data types: Arrays and objects (and functions) are complex datatypes; the rest of the datatypes are primitive. 
- * The complex datatypes are mutable and are copy by reference. The primitive datatypes are immutable and are copy by value. 
+ * The complex datatypes are mutable and are copied by reference. The primitive datatypes are immutable and are copied by value. 
  * 
  * 12: Copy by value (primitive)/Copy by reference (complex): Pimitive datatypes are copied by value while complex datatypes are copied by reference. 
- * A variable represents a reference to a value and not the value itself. 
+ * A variable represents a reference to a value and not the value itself. But for primitive datatypes, the value of a variable is stored within the variable. 
+ * See code example below for further explanation. 
  * 
- * 
- * 
- * //1// Number
- * 
- * //2// String
- * 
- * //3// Boolean
- * 
- * //4// Array
- * 
- * //5// Object
- * 
- * //6// Function
- * 
- * //7// undefined
- * 
- * //8// null 
- * 
- * //9// NaN
- * 
- * //10// Infinity and -Infinity 
- * 
- * //11// Primitive/Complex
- * 
- * //12// Copy by Value/Copy by Reference 
- * 
- * 
+ *
  */
+  
+ //1// Number
+ var a = 27;
+ var b = 23;
+ console.log(a + b); // => 60; prints the value of a plus the value of b, a basic arithmetic operation on two numbers
+ 
+ //2// String
+ var stringOne = 'This is a string';
+ var stringTwo = 'and it can be manipulated';
+ console.log(stringOne + ' ' + stringTwo); // prints 'This is a string and it can be manipulated'
+ console.log(stringOne[1]); // prints 'h', the character at the first index of stringOne
+  
+ //3// Boolean
+ console.log(1 + 1 === 2); //prints true after evaluating the logic of the statement 
+ console.log(2 + 3 === 6); //prints false
+
+ //4// Array
+ let colors = [red, orange, yellow, blue, green];
+ colors.push(indigo); //uses an array method to add indigo to the end of the array, changing the array 
+ console.log(colors[2]);//prints yellow, the color at the second index
+ 
+ //5// Object
+ let stateCapitals = {Alabama: 'Montgomery', Alaska: 'Juneau'} //an object in which the keys are states and the values are the state's capitals
+ stateCapitals.Arizona = 'Phoenix' //uses dot notation to define a new property and add it to the object, changing the object 
+ stateCapitals['Arkansas'] = 'Little Rock' //uses bracket notation to define a new property and add it to the object, changing the object
+ console.log(stateCapitals.Alabama)//uses dot notation to find the value of the capital attached to the Alabama property; prints Montgomery 
+ 
+ //6// Function
+ function checkLetters(letterOne, letterTwo){
+    if (letterOne === letterTwo){
+        console.log ('These letters are the same')
+    } console.log('These letters are different')
+ }
+
+ checkLetters(a, a) //calls the function, returns 'These letters are the same'
+ checkLetters(a, b)//calls the function, returns 'These letters are different'
+  
+ //7// undefined
+  
+ //8// null 
+  
+ //9// NaN
+  
+ //10// Infinity and -Infinity 
+  
+ //11// Primitive/Complex
+  
+ //12// Copy by Value/Copy by Reference 
+ 
+
