d641d4a820c3336afc1d1a40fa5b366b20734cb4
Victoria H <victoria.heffron@gmail.com>
2023-03-31 02:28:46 +0000

    saving progress

--- studies/datatypes.js
+++ studies/datatypes.js
@@ -1,35 +1,68 @@
 /**
  * DATATYPES
  * 
- * 0: Datatype are the categories that various values fall into. Below is a complete list of data types in JavaScript,
- * as well as explanations of how the various datatypes behave when copied. 
+ * 0: Datatypes are the categories that various values fall into. Different datatypes have different properties.
+ * Below is a list of data types in JavaScript, explanations of their respective characteristics, as well as explanations of 
+ * how the various datatypes behave when copied. 
  * 
- * 1: Number: Numbers are 
+ * 1: Number: Numbers are simply numbers. They can be positive, negative, or 0. JS can perform a multitude of arithmetic and mathematical operations
+ * on numbers.
  * 
- * 2: String: 
+ * 2: String: A string is any text wrapped by '' or "". Strings have a length property and are indexed. These characteristics mean that strings
+ *  can be manipulated. But strings are immutable, ie unchangeable, meaning that any string manipulation returns a new string rather than modifying
+ * the original string 
  * 
- * 3: Boolean:
+ * 3: Boolean: There are two Boolean values: true and false. These represent the logical outcome of conditional operations like if statements. 
  * 
- * 4: Array: 
+ * 4: Array: Arrays are complex datatypes that store data as indexed lists. They also have a length property. Beause of this, there are a variety 
+ * of methods that can be performed on them to manipulate then. They can be looped over, allowing us to perform an action on every item in the 
+ * array. They are mutable, meaning they can be changed destructively. 
  * 
- * 5: Object:
+ * 5: Object: Objects are the other complex data type, which store data as key/value pairs called properties. Referencing the object name and key name 
+ * allows us to add more values using dot notation or bracket notation.
  * 
- * 6: Function: 
+ * 6: Function: A function is not strictly a datatype, but rather is an object and is therefore a complex datatype. 
  * 
- * 7: undefined:
+ * 7: undefined: undefined is a dataype comprised of one value: undefined. It represents the absence of any value. 
  * 
- * 8: null:
+ * 8: null: null is a datatype comprised of one value: null. It is an object that represents the absence of an object.
  * 
- * 9: NaN
+ * 9: NaN: NaN stands for Not a Number, and is the result of impossible arithmetic operations. 
  * 
- * 10: Infinity and -Infinity
+ * 10: Infinity and -Infinity: Represent numbers outside of the defined range that JS can handle. Infinity is greater than any other number and negative infinity 
+ * is less than any other number. 
  * 
- * 11: Difference between primitive and complex data types: 
+ * 11: Difference between primitive and complex data types: Arrays and objects (and functions) are complex datatypes; the rest of the datatypes are primitive. 
+ * The complex datatypes are mutable and are copy by reference. The primitive datatypes are immutable and are copy by value. 
  * 
- * 12: Copy by value (primitive)/Copy by reference (complex): 
+ * 12: Copy by value (primitive)/Copy by reference (complex): Pimitive datatypes are copied by value while complex datatypes are copied by reference. 
+ * A variable represents a reference to a value and not the value itself. 
  * 
  * 
  * 
- * 2: 
+ * //1// Number
+ * 
+ * //2// String
+ * 
+ * //3// Boolean
+ * 
+ * //4// Array
+ * 
+ * //5// Object
+ * 
+ * //6// Function
+ * 
+ * //7// undefined
+ * 
+ * //8// null 
+ * 
+ * //9// NaN
+ * 
+ * //10// Infinity and -Infinity 
+ * 
+ * //11// Primitive/Complex
+ * 
+ * //12// Copy by Value/Copy by Reference 
+ * 
  * 
  */
