d3afd785ee606bfe360718c36109312face23665
Victoria H <victoria.heffron@gmail.com>
2023-04-02 23:31:59 +0000

    saving progress

--- studies/loops.js
+++ studies/loops.js
@@ -1,15 +1,74 @@
 /**
  * LOOPS
  * 
- * 0: 
+ * 0: A loop is an iterative process than run the same code block multiple times, once for each item in an array until some condition is met
+ * or once for each property of an object.
  * 
- * 1: While, for, and in loops
+ * 1: While, for, and for-in loops: While loops run as long as some condition is true. For loops iterate through an array or string, accessing 
+ * each index value in the array or string. They iterate as long as some set condition is not met, e.g. as long as the number of iterations has 
+ * not yet reached the value of the length of the array. For-in loops iterate through objects, accessing each property of the object. The syntax 
+ * of each type of loop is explained in the code examples below.
  * 
+ * 2: Loop any number of times, forward counting up to some limit, and backward counting down to 0: A common way to iterate over arrays is by starting 
+ * at the zero index and going through each index until the last one. But there are other ways to loop over arrays. We can loop a set number of times 
+ * or we can loop forward up until some limit other than the array's length. We can also count backwards from the end (or some other starting place) to the
+ * zero index (or some other stopping place).
  * 
- * 2: Looping any number of times, forward counting up to some limit, and backward counting down to 0 
+ * 3: Loop over an array, forwards and backwards: As mentioned in the previous section, we can start at the beginning of an array and loop forwards, or 
+ * start at the end of an array and loop backwards. 
  * 
- * 3: Loop over an array, forwards and backwards
- * 
- * 4: Loop over an object
+ * 4: Loop over an object: We use for-in loops to loop over objects. For-in loops allow us to access each property in an object by its key.
  * 
  */
+
+//1// While, for and for-in loops
+//While loop
+let num = 0;
+
+while (num < 35) { //if num is less than 35, run the code block below 
+    num++; //adds one to num
+}// above process repeats until num = 34
+
+
+console.log(num); // the last iteration runs when num is 34, and the code add one; prints 35
+
+//For loop
+function addOnetoOne(array) {
+    for (let i = 0; i < array.length; i++) { // i is a variable that corresponds to each index in an array, starting with 0 and changing with each iteration;
+        //i < array.length tells the for loop to run again as long as i is less than the value of the array's length; i++ means 
+        //that i's value will increase by 1 with each iteration 
+        if (array[i] === 1) { //an if statement within the for-loop; if the item at the current index is 1, run the code on the next line
+            array[i] += 1 //add 1 to the current value 
+        }
+    } return array //return the input array outside of the for loop; returning the array inside the for loop will cause the loop to 
+    //stop iterating after one run 
+}
+
+//For-in loop 
+function printValues(object) {
+    for (var key in object) { //the syntax of a for-in loop to iterate through every key/value pair in an object is for ([variable] in [input object] ){ [code]}
+        console.log(object[key] + '!') //code to be run for each property ; this example prints just the values of an object plus an exclamation point 
+    }
+} 
+
+//2// Loop any number of times
+//Count forward to a set limit 
+for (var i = 0; i <=3; i++){} //in this case the length of the array is irrelevant. The code will run 3 times, because our run condition is set to i <= 3
+
+//Count backwards to 0 
+for (var i = array.length - 1; i >= 0; i--){} //var i = array.length tells the computer to start at the end of the array; i >= 0 tells it to iterate as long
+// as i is greater than or equal to 0, and i-- tells it to count down by 1 each iteration
+
+//3// Loop forwards and backwards
+    //Loop forwards
+for (var i = 0; i <=3; i+=2){} // i+=2 tells the computer to loop forward, accessing every other index
+
+//Loop backwards 
+for (var i = array.length - 1; i >= 0; i-=2){} //i-=2 tells the computer to loop backwards, accessing every other index
+
+//4//Looping over objects
+function printValues(object) {
+    for (var key in object) { 
+        console.log(key + ': ' + object[key] + '!') //prints each property's key and value formatted as 'key: value"
+    }
+} 
--- studies/operators.js
+++ studies/operators.js
@@ -3,16 +3,168 @@
  * 
  * 0: 
  * 
+ * 
  * 1: Assignment operators: 
+ *   = Simple assignment assignment: defines a variable
+ *  += Addition assignment: adds a value to the variable
+ *  -= Subtraction assignment: subtracts a value from a variable
+ *  *= Multiplication assignment: multiplies a variable by a value
+ *  /= Division assignment: divides a variable by a value 
+ *  %= Remainder assignment: assigns a remainder to a variable; the remainder is the result after dividing the variable by some value 
+ * **= Exponentiation assignment: raises a variable to the power of a value
+ * 
  * 
  * 2: Arithmetic operators: 
+ *   + Addition: adds two values 
+ *   - Subtraction: subtracts one value from another 
+ *   * Multiplication: multiplies two values
+ *  ** Exponentiation: raises one value to the power of the other
+ *   / Division: divides one value by another
+ *   % Modulus/Remainder: returns the remainder that results from dividing two values 
+ *  ++ Increment: adds one to a value
+ *  -- Decrement: subtracts one from a value 
+ * 
  * 
  * 3. Comparison operators: 
+ *  == equal to: evaluates whether two values have the same value 
+ * === deeply equal to: evaluates whether two values have the same value and the same type
+ *  != not equal: evaluates whether two values are not equal to each other
+ * !== not equal value or not equal type: evaluates whether two values have the same value and the same type
+ *   > greater than: evaluates whether one value is greater than another
+ *   < less than: evaluates whether one value is less than another 
+ *  >= greater than or equal to: evaluates whether one value is greater than or equal to another
+ *  <= less than or equal to: evaluates whether one value is less than or equal to another 
+ * 
  * 
  * 4: Logical operators: 
+ *  && and: evaluates whether two conditions are both true 
+ *  || or: evaluates whether one of two conditions is true 
+ *
  * 
- * 5: Unary operators: (!, typeOf, -) 
+ * 5: Unary operators: (!, typeOf, -)
+ *   ! not: negates a logical outcome; ie converts true to false or false to true 
+ *  typeOf: returns a string that represents an operands data type. The possible results are undefined, null, boolean, number,
+ *      string, function, and object. 
+ *   - minus: makes a value negative 
  * 
- * 6: Ternary operator (a ? b : c) 
+ * 
+ * 6: Ternary operator 
+ *   (a ? b : c) Conditional operator: assigns a value to a variable based on some condition, can sometimes be used in place of an if statemnt
+ *      (condition ? expression to execute if truthy : expression to execute if falsey)
  * 
  */
+
+
+//1// Assignment operators 
+
+// simple assignment assignment operator
+var z = 5 // assigns variable z the value of 5
+
+// addition assignment operator
+z += 7 //adds 7 to variable z; z = 12 
+
+// subtraction assignment operator
+z -= 3 //subtracts 3 from variable z; z = 9
+
+// multiplication assignment operator
+z *= 2 //multiplies variable z by 2; z = 18
+
+// division assignment operator: 
+z /= 3 //divides variable z by 3; z = 6
+
+// remainder assignment operator
+z %= 2 //divides the variable z by 2, finds the remainders, then assigns the remainder value to z; z = 3
+
+
+//2// Arithmetic operators
+var x = 14
+var y = 7
+//addition
+var xAndY = x + y // = 21
+
+//subtraction
+var xMinusY = x - y // = 7
+
+//multiplication
+var xTimesY = x * y // = 98
+
+//exponentiation
+var xToTheY = x ** y // = 105413504
+
+//division
+var xDividedByY = x / y // = 2
+
+//modulus/remainder
+var remainderXDividedByY = x % y // = 0
+
+//increment
+var xPlusOne = x++ // = 2
+
+//decrement
+var yMinusOne = y-- // = 13
+
+
+//3// Comparison operators 
+var a = 5
+var b = '5'
+var c = 7
+ 
+//equal to
+a == b //true; 5 has the same value as '5'
+
+//deeply equal to 
+a === b //false; 5 has the same value as but different type than '5' 
+
+//not equal
+a != c //true, 5 does not equal 7
+
+//not equal value or not equal type
+a !== b //true, 5 equals '5' in value but not in type
+
+//greater than
+c > a //true; 7 is greater than 5
+
+//less than 
+c < a //false; 7 is not less than 5
+
+//greater than or equal to 
+a + 2 >= c //true; 5 + 2 is greater than or equal to 7
+
+//less than or equal to 
+a <= c // true; 5 is less than or equal to 7 
+
+
+//4// Logical operators
+let d = 4 
+let e = 5
+let f = 6
+
+//and
+d < f && e < f //true; 4 is less than 6 and 5 is less than 6 
+e < f && f < d //false, 5 is less than 6 but 6 is not less than 4
+
+//or
+f < e || d < e //true; 6 is not less than 5 but 4 is less than 6
+f < e || e < d //false; 6 is not less than 5 and 5 is not less than 6
+
+
+ //5// Unary operators 
+ let g = 1
+ let h = 2
+
+ //not 
+ !(g < h) // false; d is less than f, so the logic resolves to not true, ie false 
+
+ //typeOf 
+ typeOf(42) //returns 'number'
+ typeOf('some words') //returns string
+ typeOf(17) === 'string' //false; 17 is not a string
+ 
+ //minus
+ var negativeG = -g // negativeG = -1
+
+ //6// Ternary operator
+ function printGreeting(time){ //function takes an input of hour that represents the hour of the day, 0-24
+    (time > 4 && time < 20 ? console.log('hello!') : console.log('good night!') ) //if the time is between 4 am and 8 pm, print 'hello'! otherwise, print 'good night!'
+ }
+ 
